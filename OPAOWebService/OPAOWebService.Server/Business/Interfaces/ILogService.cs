3d702539c32c3c6299d7816e6af07abc4aa5a43d
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-04-30 11:04:08 -0500

    Added ILogService

---
+++ OPAOWebService/OPAOWebService.Server/Business/Interfaces/ILogService.cs
@@ -0,0 +1,25 @@
+﻿using OPAOWebService.Server.Models.DTOs.Requests;
+
+namespace OPAOWebService.Server.Business.Interfaces
+{
+    /// <summary>
+    /// Defines business logic operations for processing tax-related property data.
+    /// </summary>
+    /// <remarks>
+    /// <para><strong>Author:</strong> Joseph Adogeri</para>
+    /// <para><strong>Since:</strong> 23-APR-2026</para>
+    /// <para><strong>Version:</strong> 1.1.0</para>
+    /// <para><strong>File:</strong> ITaxService.cs</para>
+    /// </remarks>
+
+    public interface ITaxService
+    {
+
+        /// <summary>
+        /// Updates the valuation status of a property parcel.
+        /// </summary>
+        /// <param name="request">The assessment update details.</param>
+        /// <returns>An integer status code (e.g., 1 for success, 0 for failure).</returns>
+        int UpdatePropertyValuation(AssessmentStatusRequest request);
+    }
+}
